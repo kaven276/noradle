bdc0673f5534c0e7d3330f8314bd3d39a877ca93
Li Yong <kaven276@vip.sina.com>
2012-03-20 16:24:07 +0800

    user create or replace context

--- oracle/contexts.sql
+++ oracle/contexts.sql
@@ -1,3 +1,3 @@
-create context sess_attr_ctx using k_gac accessed globally;
-create context sess_cid_ctx using k_gac accessed globally;
-create context key_ver_ctx using k_gac accessed globally;
+create or replace context sess_attr_ctx using k_gac accessed globally;
+create or replace context sess_cid_ctx using k_gac accessed globally;
+create or replace context key_ver_ctx using k_gac accessed globally;
