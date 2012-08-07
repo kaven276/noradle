2a6e08ee9d088926b1902a36bf7d7780c9aae79c
Li Yong <kaven276@vip.sina.com>
2012-08-07 15:36:05 +0800

    add ua context to store map of (ua hash,ua string)

--- oracle/contexts.sql
+++ oracle/contexts.sql
@@ -1,3 +1,4 @@
 create or replace context sess_attr_ctx using k_gac accessed globally;
 create or replace context sess_cid_ctx using k_gac accessed globally;
 create or replace context key_ver_ctx using k_gac accessed globally;
+create or replace context ua using r accessed globally;
