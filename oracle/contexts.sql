67d84e596fbeb34dfa52ad455cf162f5a26ae57c
Li Yong <kaven276@vip.sina.com>
2012-08-08 20:15:00 +0800

    GAC sess_attr_ctx is no longer needed

--- oracle/contexts.sql
+++ oracle/contexts.sql
@@ -1,4 +1,3 @@
-create or replace context sess_attr_ctx using k_gac accessed globally;
 create or replace context sess_cid_ctx using k_gac accessed globally;
 create or replace context key_ver_ctx using k_gac accessed globally;
 create or replace context ua_ctx using r accessed globally;
