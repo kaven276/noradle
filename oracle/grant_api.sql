7c032bf57b5251d0752b4492150c3caefa2fc623
Li Yong <kaven276@vip.sina.com>
2013-03-27 16:19:07 +0800

    grant_api shortcut format bugfix

--- oracle/grant_api.sql
+++ oracle/grant_api.sql
@@ -8,17 +8,17 @@ grant execute on rb to public;
 grant execute on u to public;
 grant execute on e to public;
 grant execute on g to public;
-grant execute on k_http to public; -- h
-grant execute on k_xhtp to public; -- p
-grant execute on k_type_tool to public; -- t
+grant execute on k_http to public -- h;
+grant execute on k_xhtp to public -- p;
+grant execute on k_type_tool to public -- t;
 grant execute on k_gw to public;
 grant execute on k_bg to public;
 grant execute on k_filter to public;
 grant all on ext_url_v to public;
-grant execute on k_debug to public; -- odb
-grant execute on k_sess to public; -- s
+grant execute on k_debug to public -- odb;
+grant execute on k_sess to public -- s;
 grant execute on kv to public;
 grant execute on rs to public;
-grant execute on k_ext_call to public; -- dco
+grant execute on k_ext_call to public -- dco;
 grant execute on pdu to public;
-grant execute on k_gac to public; -- gac
+grant execute on k_gac to public -- gac;
