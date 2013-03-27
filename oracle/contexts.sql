d6d3330b2804b7157c48e5cdf4a8a7e9f9304ffc
Li Yong <kaven276@vip.sina.com>
2013-03-27 10:52:24 +0800

    rm ua cache

--- oracle/contexts.sql
+++ oracle/contexts.sql
@@ -1,3 +1,3 @@
 create or replace context sess_cid_ctx using k_gc accessed globally;
 create or replace context key_ver_ctx using kv accessed globally;
-create or replace context ua_ctx using r accessed globally;
+-- create or replace context ua_ctx using r accessed globally;
