726138b20559aca9d3f6877e45281b671f384200
Li Yong <kaven276@vip.sina.com>
2013-03-27 10:53:00 +0800

    rm select any table,connect from psp

--- oracle/grant2psp.sql
+++ oracle/grant2psp.sql
@@ -1,7 +1,7 @@
 grant execute on SYS.PW to &pspdbu;
 grant read, write on directory SYS.PLSHPROF_DIR to PSP;
 
--- Grant/Revoke object privileges 
+-- Grant/Revoke object privileges
 grant execute on SYS.DBMS_ALERT to &pspdbu;
 grant execute on SYS.DBMS_CRYPTO to &pspdbu;
 grant execute on SYS.DBMS_LOCK to &pspdbu;
@@ -9,9 +9,9 @@ grant execute on SYS.DBMS_OBFUSCATION_TOOLKIT_FFI to &pspdbu;
 grant execute on SYS.DBMS_PIPE to &pspdbu;
 grant execute on SYS.DBMS_SCHEDULER to &pspdbu;
 grant execute on SYS.UTL_TCP to &pspdbu;
+grant execute on SYS.DBMS_HPROF to PSP;
 
--- Grant/Revoke role privileges 
-grant connect to &pspdbu;
+-- Grant/Revoke role privileges
 grant resource to &pspdbu;
 
 -- Grant/Revoke system privileges 
@@ -20,4 +20,3 @@ grant create job to &pspdbu;
 grant create session to &pspdbu;
 grant create view to &pspdbu;
 grant select any dictionary to &pspdbu;
-grant select any table to &pspdbu;
