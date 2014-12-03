ebc93c8aca6609c18c4aa08beed8bb4775d04a97
Li Yong <kaven276@vip.sina.com>
2014-12-03 10:52:55 +0800

    add server process bound GAC, see req start_time

--- oracle/contexts.sql
+++ oracle/contexts.sql
@@ -1,3 +1,4 @@
-create or replace context sess_cid_ctx using k_gc accessed globally;
+create or replace context gac_speed_test using dbms_session accessed globally;
 create or replace context key_ver_ctx using kv accessed globally;
+create or replace context server_process using dbms_session accessed globally;
 -- create or replace context ua_ctx using r accessed globally;
