d63cd8a06e818e771c12470ddb36ee9ca508f266
Li Yong <kaven276@vip.sina.com>
2015-01-08 14:39:01 +0800

    reorder synonym/grant items

--- oracle/grant_api.sql
+++ oracle/grant_api.sql
@@ -1,27 +1,33 @@
+grant execute on k_gw to public;
+grant execute on k_filter to public;
+
 grant execute on st to public;
 grant execute on nt to public;
 grant execute on tmp to public;
+grant execute on k_type_tool to public -- t;
+
+grant execute on k_debug to public -- odb;
+grant execute on kv to public;
+
 grant execute on r to public;
 grant execute on ra to public;
 grant execute on rb to public;
+
+grant all on ext_url_v to public;
 grant execute on u to public;
+grant execute on url to public -- l;
 grant execute on e to public;
 grant execute on g to public;
 grant execute on k_http to public -- h;
 grant execute on k_xhtp to public -- p;
-grant execute on k_type_tool to public -- t;
-grant execute on k_gw to public;
-grant execute on k_filter to public;
-grant all on ext_url_v to public;
-grant execute on k_debug to public -- odb;
-grant execute on kv to public;
 grant execute on rs to public;
+
 grant execute on tag to public -- x;
 grant execute on multi to public -- m;
 grant execute on tagp to public -- tgp;
 grant execute on attr to public -- atr;
-grant execute on url to public -- l;
 grant execute on style to public; -- sty
 grant execute on list to public;
 grant execute on tree to public;
+
 grant execute on msg_pipe to public;
--- oracle/pub_synonym.sql
+++ oracle/pub_synonym.sql
@@ -1,24 +1,38 @@
+prompt framework
+create or replace public synonym k_gw for k_gw;
+create or replace public synonym k_filter for k_filter;
+
+prompt tools
 create or replace public synonym st for st;
 create or replace public synonym nt for nt;
 create or replace public synonym tmp for tmp;
+create or replace public synonym t for k_type_tool;
+
+prompt other tools
+create or replace public synonym k_debug for k_debug;
+create or replace public synonym odb for k_debug;
+create or replace public synonym kv for kv;
+
+prompt for input/request
 create or replace public synonym r for r;
 create or replace public synonym ra for ra;
 create or replace public synonym rb for rb;
-create or replace public synonym u for u;
-create or replace public synonym e for e;
+
+prompt for output/response
+create or replace public synonym ext_url_v for ext_url_v;
+create or replace public synonym u for u; -- tobe removed
+create or replace public synonym l for url;
+create or replace public synonym e for e; -- tobe removed
 create or replace public synonym g for g;
 create or replace public synonym h for k_http;
-create or replace public synonym p for k_xhtp;
-create or replace public synonym t for k_type_tool;
-create or replace public synonym k_gw for k_gw;
-create or replace public synonym k_filter for k_filter;
-create or replace public synonym ext_url_v for ext_url_v;
-create or replace public synonym k_debug for k_debug;
-create or replace public synonym kv for kv;
+create or replace public synonym p for k_xhtp; -- tobe removed
 create or replace public synonym rs for rs;
 
-create or replace public synonym odb for k_debug;
-
+prompt for advance output/response (tag print)
+create or replace public synonym style for style;
+create or replace public synonym sty for style;
+create or replace public synonym c for style;
+create or replace public synonym y for style;
 create or replace public synonym tag for tag;
 create or replace public synonym x for tag;
 create or replace public synonym multi for multi;
@@ -27,16 +41,12 @@ create or replace public synonym tagp for tagp;
 create or replace public synonym tgp for tagp;
 create or replace public synonym attr for attr;
 create or replace public synonym atr for attr;
--- create or replace public synonym url for url;
-create or replace public synonym l for url;
-create or replace public synonym style for style;
-create or replace public synonym sty for style;
-create or replace public synonym c for style;
-create or replace public synonym y for style;
 create or replace public synonym list for list;
 create or replace public synonym tl for list;
 create or replace public synonym tree for tree;
 create or replace public synonym tr for tree;
+
+prompt for call-out request/response
 create or replace public synonym msg_pipe for msg_pipe;
 create or replace public synonym mp for msg_pipe;
 create or replace public synonym mp_h for msg_pipe;
