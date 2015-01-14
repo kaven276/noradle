385188f39b13644f77a6408b6c2badfc832c34e4
Li Yong <kaven276@vip.sina.com>
2015-01-14 15:54:25 +0800

    check units support demo and user_objects diff

--- oracle/check_units.sh
+++ oracle/check_units.sh
@@ -1,8 +1,11 @@
 # compare units in ./psp/install_psp.sql and units in directory ./psp
 
 cat psp/install_psp_obj.sql | grep @@ | cut -c 3- | tr -d "\r" | sort > ./unit_list.txt
+# pbpaste > ./unit_list.txt
 git add ./unit_list.txt
+
 ls  psp | grep -v "install_psp_obj.sql" | sort > ./unit_list.txt
+# ls  psp | grep -v "install_psp_obj.sql"| cut -d . -f 1 | sort > ./unit_list.txt
 git diff -- ./unit_list.txt
 
 git checkout -- ./unit_list.txt
