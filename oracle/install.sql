2acfaba35235710cb6ba0e6940ed29ac0fc528b5
Li Yong <kaven276@vip.sina.com>
2015-01-21 17:08:44 +0800

    install script for demo moved to noradle-demo

--- oracle/install.sql
+++ oracle/install.sql
@@ -8,8 +8,6 @@ whenever sqlerror exit
 
 rem drop user psp cascade;
 rem create user psp identified by psp default tablespace sysaux temporary tablespace temp;
-rem drop user demo cascade;
-rem create user demo identified by demo default tablespace sysaux temporary tablespace temp;
 
 set define on
 
@@ -71,32 +69,12 @@ exec DBMS_UTILITY.COMPILE_SCHEMA(upper('&pspdbu'),false);
 
 --------------------------------------------------------------------------------
 
-prompt Are you sure you have clean empty DEMO db user/schema already?
-prompt Noradle demo's units(tables,plsql,...) in oracle will be installed to the schema
-prompt You can try the sql scripts below to achieve the preparation required above.
-prompt drop user demo cascade;;
-prompt create user demo identified by demo default tablespace users;;
-pause if not, create empty DEMO db users beforehand, and then press enter to continue
-accept demodbu char default 'demo' prompt 'Enter the schema/User(must already exist) for noradle demo (demo) : '
-
-prompt Installing Noracle(psp.web) demo app to schema "&demodbu"
-pause press enter to continue ...
-alter session set current_schema = &demodbu;
-@@grant2demo.sql
-
-prompt begin to install Noradle demo schema objects
-@@../demo/schema/install_demo_obj.sql
-exec DBMS_UTILITY.COMPILE_SCHEMA(upper('&demodbu'),false);
-
-whenever sqlerror continue
-insert into ext_url_v(key,prefix) values('myself','/f');
-commit;
-whenever sqlerror exit
-
 prompt Noradle bundle in oracle db part have been installed successfully!
 prompt Please follow the steps below to learn from demo
+prompt 0. first, install noradle-demo
 prompt 1. config server_config_t, let oracle known where to reverse connect nodejs
 prompt 2. run nodejs server, quick start with default cfg by "cd demo", "npm start"
 prompt 3. in oracle psp schema, exec "k_pmon.run_job" to start processes to serv.
 prompt 4. in your browser, access "http://localhost:8888/demo" (for example) to see the demo
 spool off
+exit
