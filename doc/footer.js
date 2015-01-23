0688100dc2f5c74222fecbd04f829d5388b43ec5
Li Yong <kaven276@vip.sina.com>
2015-01-23 11:25:27 +0800

    update noradle banner color

--- doc/footer.js
+++ doc/footer.js
@@ -1,3 +1,3 @@
-document.write('<hr/><div class="footer"><span class="psp_logo"><b>NO</b><i>RA</i><b>D</b><i>L</i><b>E</b></span></div>');
+document.write('<hr/><div class="footer"><span class="psp_logo"><b>N</b><s>O</s></b><i>RA</i><b>D</b><i>L</i><s>E</s></span></div>');
 // document.title = document.getElementById('title').innerText;
 // document.write('</body></html>');
