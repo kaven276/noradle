ef46b760566b6b82509780d8337ae61d6a55a41f
Li Yong <kaven276@vip.sina.com>
2015-09-24 09:30:52 +0800

    remove doc from git version control

--- doc/footer.js
+++
@@ -1,3 +0,0 @@
-document.write('<hr/><div class="footer"><span class="psp_logo"><b>N</b><s>O</s></b><i>RA</i><b>D</b><i>L</i><s>E</s></span></div>');
-// document.title = document.getElementById('title').innerText;
-// document.write('</body></html>');
--- doc/header.js
+++
@@ -1,5 +0,0 @@
-// document.write('!5');
-// document.write('<html><body>');
-document.write('<link type="text/css" rel="stylesheet" href="doc.css" />');
-document.write('<span class="psp_logo"><b>N</b><s>O</s></b><i>RA</i><b>D</b><i>L</i><s>E</s></span><hr/>');
-
